3454bb00ed453cde8bb3c324bf1a3da1d79c0f0d
Sean Tully <stully17@gmail.com>
2025-11-14 13:59:40 -0500

    Generates a random dice rolls

---
+++ random_roll.py
@@ -0,0 +1,19 @@
+def roll(rolls):
+    """ 
+    Summary: 
+        Generates random numbers between 1-6 to simulate a dice roll 
+        stores all the randomly generated numbers in a list to be returned 
+    
+    Args:
+        rolls (int): The amount of random dice rolls you would like
+
+    Returns:
+        List: List with all the randomly rolled dice values in each index
+    """
+    import random
+    back = list()
+    count = 0
+    while count < int(rolls):
+           back.append(random.randint(1,6))
+           count+=1
+    return back
