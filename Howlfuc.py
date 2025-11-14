a8ae602217776b2ed58349f85759845e69994587
sameqsmas <Sammy97boss@gmail.com>
2025-11-14 16:02:13 -0500

    Final changes and temp class implementations

--- Howlfuc.py
+++ Howlfuc.py
@@ -1,11 +1,30 @@
 import random
 
-class player():
+def player_class():
     '''
     Temporary player class to be implemented.
-    
+    This functions is not final 
     '''
+    player_class = [] # list of player cards
+    return player_class
+
+
+def card_class(new_card_dice):
+    """
+    Card class that is a composition from the player class. 
+    
+    """
+    
+
+def deal():
+    """
+    A method of the card class to be implemented in the future, It will chose a random variable 
+    name from a list of name and generate a random number of die between 1-4
+    and values for each dice. 
+    """
     pass
+
+
 
 def howl(player):
     '''
