801b2848d839a5416b9691a09e33f97ee2c37243
Sean Tully <stully17@gmail.com>
2025-12-06 15:40:24 -0500

    Updated version of bust function  -Sean T

--- bust.py
+++ bust.py
@@ -1,3 +1,10 @@
 """Function meant to track and clear all uncompleted and unlocked dog cards and 
 yard when a player busts
 """
+def bust (player):
+    total = 0
+    for dye in player.yard:
+        total+=dye
+    if total > 7:
+        player.yard = list()
+        player.active_cards = list()
