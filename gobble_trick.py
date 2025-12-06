bc7e3bd1bd9e7f7cabf3fc5f81c44ac1e7ea1e4e
Mackenzie Barrett <mbarret1@terpmail.umd.edu>
2025-12-06 16:35:03 -0500

    Adjusted gobble doc string

--- gobble_trick.py
+++ gobble_trick.py
@@ -4,9 +4,6 @@ Take 7 treats
 THEN
 Return 1 treat for each spot in your highest unfilled space
 
-(suggestion: maybe do a set intersection of the cards' required dice and 
-fulfilled dice and then list sorting to return the highest overall required dice
-from a list of all the uncompleted cards the player has)
 """
 def gobble_trick(player):
     treat = 7
