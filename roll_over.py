ec67b43c2ca3e6d237d861622303e063666e6352
Sean Tully <stully17@gmail.com>
2025-12-06 16:57:16 -0500

    Updated roll_over edited still needs work -Sean T

--- roll_over.py
+++ roll_over.py
@@ -4,3 +4,10 @@ Roll all your buried dice and then place or rebury them
 THEN
 You may roll 1 die
 """
+import random_roll
+def roll_over(player):
+    hold = random_roll(len(player.yard))
+    # Place Holder for dice_placement()
+    random_roll(1)
+    # Place Holder for dice_placement()
+    
