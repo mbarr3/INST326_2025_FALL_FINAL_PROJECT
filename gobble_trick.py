a43af3b6c3e8118e21b5daa9c2f56ff460093106
Sean Tully <stully17@gmail.com>
2025-12-06 16:20:42 -0500

    Updated version of gobble_trick function  -Sean T

--- gobble_trick.py
+++ gobble_trick.py
@@ -8,3 +8,12 @@ Return 1 treat for each spot in your highest unfilled space
 fulfilled dice and then list sorting to return the highest overall required dice
 from a list of all the uncompleted cards the player has)
 """
+def gobble_trick(player):
+    treat = 7
+    blank_max = 0
+    for card in player.active_cards:
+        for dice in card.req_dice:
+            if not card.req_dice[dice]:
+                blank_max = dice 
+    player.treats = treat - blank_max
+            
