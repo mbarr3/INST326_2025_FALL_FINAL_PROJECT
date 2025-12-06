3b276651d518615aad5253206b560b2cd92a8fd8
Sean Tully <stully17@gmail.com>
2025-12-06 17:23:03 -0500

    updated chase trick

--- chase_trick.py
+++ chase_trick.py
@@ -5,4 +5,17 @@ Roll 1 die
 You may repeat this trick as many times as you want but each time roll 1 more 
 die than you just did (2, 3, 4...)
 """
+import random_roll, dice_placment
+def chase_trick(player):
+    while True:
+        count = 1
+        hold = random_roll(count)
+        dice_placment(player,hold)
+        inp = input("would you like to roll again")
+        if inp == "n":
+            break
+    
+    
+        
+    
 
