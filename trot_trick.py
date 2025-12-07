b95913c1d34df1fc49d92de2558007d870d36511
sameqsmas <Sammy97boss@gmail.com>
2025-12-07 14:11:57 -0500

    My trot function - Sam O

--- trot_trick.py
+++ trot_trick.py
@@ -1,3 +1,7 @@
+import random_roll as rr
+import dice_placement as dp
+from Spots import Player, Card
+
 """Function for the trot trick
 
 You may move 1 die on your dog cards to any other space, changing the number if 
@@ -5,3 +9,24 @@ needed
 THEN 
 Roll 2 dice
 """
+
+def trot(Player,Selected_die):
+    old_card = None
+    old_place = None
+    
+    for Card in Player.active_cards:
+        for place, value in Card.req_dice_items():
+            if value == Selected_die:
+               old_card = value
+               old_place = old_place
+               break
+    if old_card is None:
+        print("Selected card doesn't exist")
+        return
+    
+    old_card.req_dice[old_place] = None
+    dp.dice_placement(Player, [Selected_die])
+    
+    
+            
+    rr.roll(2)
