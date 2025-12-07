fbadf0163e9abe4a61fddac07a210cfd4a9d01dd
Mackenzie Barrett <mbarret1@terpmail.umd.edu>
2025-12-07 14:58:54 -0500

    Continued progress of dice placement

--- dice_placement.py
+++ dice_placement.py
@@ -1,3 +1,5 @@
+import bust
+
 
 def dice_placement(player, dice_list):
     """Takes a list of dice values and determines if they can be placed on any
@@ -23,13 +25,11 @@ def dice_placement(player, dice_list):
                                         placement option
             bury_dice (list of ints): list of die values determined to be placed
                                     in the player's yard
-            Updates the Player class obj and Card class obj attributes:
-                player.yard
-                card.req_values
+
     Returns:
     
     Author: Mackenzie Barrett
-    Technique: Sequence Unpacking
+    Technique: 
     """
     placement_dict = {}
     prompt_list = []
@@ -45,19 +45,41 @@ def dice_placement(player, dice_list):
                     placement_dict[key].add(card)
     
     # Place die automatically on a card or in the yard OR prompt player to choose placement if multiple options   
-    for key, value in placement_dict.keys():
+    for key in placement_dict.keys():
         # Place any values with multiple placement options in a list
         if len(placement_dict[key]) > 1:
-            prompt_list.append([key, value])
+            prompt_list.append(key)
         # Automatically place die values on a card if only one placement is available
         elif len(placement_dict[key]) == 1:
             open_card = placement_dict[key]
+            open_card.req_dice[key] = key
         # Die values with no possible placement go to the player's yard
         elif len(placement_dict[key]) == 0:
             # Die must go in a list to go to the yard
             bury_dice.append(key)
+    
+    # Prompt player for dice placement
+    print(f"{prompt_list}\nThese dice values have more than one spot they can"\
+        f" be placed.")
+    for die in prompt_list:
+        while True:
+            print(f"Your card options are:\t{placement_dict[die]}")
+            # TODO Need to figure out how Card class obj printing will work and 
+            # how to translate player input to card obj name for the next step
+            selection = input(f"Of the cards above which one would you like to"\
+                f" place the {die} on?\t")
+            # TODO Validate input (unsure yet how )
+            if selection not in placement_dict[die]:
+                print(f"{selection} is not one of the available cards listed above")
+                continue
+            else:
+                break
             
-            
-            
+    # Add bury_list to the player's yard
+    for die in bury_dice:
+        player.yard.append(die)
+        
+    # Check if the player has busted
+    bust(player)
                 
         
